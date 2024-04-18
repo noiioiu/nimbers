3ac051d6627534ea8c38a5987a92b75a515623d5
noiioiu <noiioiu@cocaine.ninja>
2024-04-18 12:46:31 -0500

    Add test for involutivity

--- test/Main.hs
+++ test/Main.hs
@@ -11,7 +11,7 @@ instance Arbitrary Nimber where
   arbitrary = fromInteger <$> arbitrary
 
 prop_neg :: Nimber -> Bool
-prop_neg a = a + a == 0
+prop_neg a = a - a == 0
 
 prop_assoc_add :: Nimber -> Nimber -> Nimber -> Bool
 prop_assoc_add a b c = a + (b + c) == (a + b) + c
@@ -25,8 +25,11 @@ prop_distrib a b c = a * (b + c) == a * b + a * c
 prop_recip :: Nimber -> Bool
 prop_recip a = a == 0 || a / a == 1
 
+prop_inv :: Nimber -> Bool
+prop_inv a = a == 0 || recip (recip a) == a
+
 main :: IO ()
-main = check prop_neg >> check prop_assoc_add >> check prop_assoc_mul >> check prop_distrib >> check prop_recip
+main = check prop_neg >> check prop_assoc_add >> check prop_assoc_mul >> check prop_distrib >> check prop_recip >> check prop_inv
   where
     check prop = do
       result <- verboseCheckResult prop
