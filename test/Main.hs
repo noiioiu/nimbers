11c47988c946e47bcd55f7158a20199763570d38
noiioiu <noiioiu@cocaine.ninja>
2024-04-21 09:12:27 -0500

    Move definitions to test

--- test/Main.hs
+++ test/Main.hs
@@ -2,6 +2,7 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Evaluate" #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
 module Main (main) where
 
@@ -37,6 +38,27 @@ prop_recip a = a == 0 || a / a == 1
 
 prop_inv :: Nimber -> Bool
 prop_inv a = a == 0 || recip (recip a) == a
+
+mex :: (Functor f, Foldable f, Eq b, Num b, Num a) => f b -> a
+mex s = if 0 `notElem` s then 0 else 1 + mex (fmap (+ (-1)) s)
+
+-- | Compute nimber sum directly from the definition.  This is very slow.
+nimberAdd :: Int -> Int -> Int
+nimberAdd = (!!) . (nimberSumTable !!)
+
+nimberSumTable :: [[Int]]
+nimberSumTable = fmap add <$> [(i,) <$> [0 ..] | i <- [0 ..]]
+  where
+    add (a, b) = mex $ [nimberSumTable !! a' !! b | a' <- [0 .. a - 1]] ++ [nimberSumTable !! a !! b' | b' <- [0 .. b - 1]]
+
+-- | Compute nimber product directly from the definition.  This is very slow.
+nimberMul :: Int -> Int -> Int
+nimberMul = (!!) . (nimberProdTable !!)
+
+nimberProdTable :: [[Int]]
+nimberProdTable = fmap mul <$> [(i,) <$> [0 ..] | i <- [0 ..]]
+  where
+    mul (a, b) = mex $ [(nimberProdTable !! a' !! b) `nimberAdd` (nimberProdTable !! a !! b') `nimberAdd` (nimberProdTable !! a' !! b') | a' <- [0 .. a - 1], b' <- [0 .. b - 1]]
 
 pure []
 runTests :: IO Bool
