3c21ba4120ce10e4113a695dcd084c3aee1f0537
noiioiu <noiioiu@cocaine.ninja>
2024-04-25 09:39:29 -0500

    Rewrite some functions to avoid recomputing log^2.
    Add Artin-Schreier root function.
    
    Add tests.

--- src/Data/Nimber.hs
+++ src/Data/Nimber.hs
@@ -43,13 +43,19 @@ instance Num Nimber where
   a * 1 = a
   a * b =
     let m = max (floorLog @Int (floorLog a)) (floorLog @Int (floorLog b)) -- D = 2^2^m is the largest Fermat 2-power less than or equal to both a and b
-        semiD = bit (bit m - 1) -- semimultiple of D
-        a1 = a `shiftR` bit m -- a = a1D+a2
-        a2 = a .^. (a1 `shiftL` bit m)
-        b1 = b `shiftR` bit m -- b = b1D+b2
-        b2 = b .^. (b1 `shiftL` bit m)
-        c = a2 * b2
-     in (((a1 + a2) * (b1 + b2) - c) `shiftL` bit m) + a1 * b1 * semiD + c
+        mult' _ 0 _ = 0
+        mult' _ _ 0 = 0
+        mult' _ 1 t = t
+        mult' _ s 1 = s
+        mult' k s t =
+          let semiD = bit (bit k - 1) -- semimultiple of D
+              s1 = s `shiftR` bit k -- a = a1D+a2
+              s2 = s .^. (s1 `shiftL` bit k)
+              t1 = t `shiftR` bit k -- b = b1D+b2
+              t2 = t .^. (t1 `shiftL` bit k)
+              c = mult' (k-1) s2 t2
+           in ((mult' (k-1) (s1 + s2) (t1 + t2) - c) `shiftL` bit k) + mult' (k-1) (mult' (k-1) s1 t1) semiD + c
+     in mult' m a b
   negate = id
   abs = id
   signum = id
@@ -60,22 +66,22 @@ sqr 0 = 0
 sqr 1 = 1
 sqr n =
   let m = floorLog @Int $ floorLog n -- D = 2^2^m is the largest Fermat 2-power less than or equal to n
-      a = n `shiftR` bit m -- n = aD+b
-      aD = a `shiftL` bit m
-      b = n .^. aD
-      semiD = bit (bit m - 1) -- semimultiple of D
-      sqra = sqr a
-   in sqra `shiftL` bit m + sqra * semiD + sqr b
+      sqr' _ 0 = 0
+      sqr' _ 1 = 1
+      sqr' k l =
+        let a = l `shiftR` bit k -- n = aD+b
+            aD = a `shiftL` bit k
+            b = l .^. aD
+            semiD = bit (bit k - 1) -- semimultiple of D
+            sqra = sqr' (k - 1) a
+         in sqra `shiftL` bit k + sqra * semiD + sqr' (k - 1) b
+   in sqr' m n
 
 -- | Raise a @'Nimber'@ to an integral power.  Faster than using '^' or '^^'.
-pow :: (Integral a) => Nimber -> a -> Nimber
-_ `pow` 0 = 1
-a `pow` n
-  | n < 0 = recip a `pow` negate n
-  | otherwise = if even n then sqr a `pow` quot n 2 else powAcc (sqr a) (quot n 2) a
-  where
-    powAcc _ 0 x = x
-    powAcc b m x = if even m then powAcc (sqr b) (quot m 2) x else powAcc (sqr b) (quot m 2) (b * x)
+pow :: (Integral a, Bits a) => Nimber -> a -> Nimber
+x `pow` n
+  | n < 0 = recip x `pow` negate n
+  | otherwise = product . fmap snd . filter (testBit n . fst) . zip [0 ..] . take (1 + floorLog (n + 1)) $ iterate sqr x
 
 -- | The finite nimbers are a field of characteristic 2.  There is no field homomorphism from the rationals to the nimbers, so @'fromRational'@ is always an error.
 instance Fractional Nimber where
@@ -84,11 +90,14 @@ instance Fractional Nimber where
   recip 1 = 1
   recip n =
     let m = floorLog @Int $ floorLog n -- D = 2^2^m is the largest Fermat 2-power less than or equal to n
-        a = n `shiftR` bit m -- n = aD+b
-        aD = a `shiftL` bit m
-        b = n .^. aD
-        semiD = bit (bit m - 1) -- semimultiple of D
-     in (aD + a + b) / (semiD * sqr a + b * (a + b))
+        recip' _ 1 = 1
+        recip' k l =
+          let a = l `shiftR` bit k -- n = aD+b
+              aD = a `shiftL` bit k
+              b = l .^. aD
+              semiD = bit (bit k - 1) -- semimultiple of D
+           in (aD + a + b) * recip' (k - 1) (semiD * sqr a + b * (a + b))
+     in recip' m n
 
 -- | The only reason this instance exists is to define square roots.  None of the other @'Floating'@ methods apply to @'Nimber'@s.
 instance Floating Nimber where
@@ -96,12 +105,16 @@ instance Floating Nimber where
   sqrt 1 = 1
   sqrt n =
     let m = floorLog @Int $ floorLog n -- D = 2^2^m is the largest Fermat 2-power less than or equal to n
-        a = n `shiftR` bit m -- n = aD+b
-        aD = a `shiftL` bit m
-        b = n .^. aD
-        semiD = bit (bit m - 1) -- semimultiple of D
-        sqrta = sqrt a
-     in sqrta `shiftL` bit m + sqrta * sqrt semiD + sqrt b
+        sqrt' _ 0 = 0
+        sqrt' _ 1 = 1
+        sqrt' k l =
+          let a = l `shiftR` bit k -- n = aD+b
+              aD = a `shiftL` bit k
+              b = l .^. aD
+              semiD = bit (bit k - 1) -- semimultiple of D
+              sqrta = sqrt' (k - 1) a
+           in sqrta `shiftL` bit k + sqrta * sqrt semiD + sqrt' (k - 1) b
+     in sqrt' m n
   pi = error "π is not a nimber"
   exp _ = error "exp undefined for nimbers"
   log _ = error "log undefined for nimbers"
@@ -117,3 +130,39 @@ instance Floating Nimber where
   asinh _ = error "Hyperbolic functions undefined for nimbers"
   acosh _ = error "Hyperbolic functions undefined for nimbers"
   atanh _ = error "Hyperbolic functions undefined for nimbers"
+
+evens :: [a] -> [a]
+evens (x : _ : xs) = x : evens xs
+evens xs = xs
+
+-- | @'artinSchreierRoot' n@ is the smallest solution to the equation \(x^2 - x = n\).
+--   The algorithm is due to Chin-Long Chen: <https://ieeexplore.ieee.org/document/1056557>.
+--   In fields of characteristic 2, the standard quadratic formula does not work, but any quadratic equation can be solved using square roots and Artin-Schreier roots.
+--
+--   This function is __much__ slower than @'sqrt'@.
+artinSchreierRoot :: Nimber -> Nimber
+artinSchreierRoot 0 = 0
+artinSchreierRoot 1 = 2
+artinSchreierRoot 2 = 4
+artinSchreierRoot 3 = 6
+artinSchreierRoot n =
+  let m = 1 + floorLog @Int (floorLog n) -- 2^2^m is the order of the smallest field containing n
+      m' = if n < bit (bit m - 1) then m else m + 1 -- 2^2^m' is the order of the smallest field containing the Artin-Schreier root of n
+      squares = iterate sqr n
+      quarts = evens squares
+      t4k = sum $ take (bit (m' - 1)) quarts -- trace of the Artin-Screier root of n
+   in if t4k == 1
+        then
+          let s = sum $ do
+                j <- [1 .. bit (m' - 2) - 1]
+                i <- [j .. bit (m' - 2) - 1]
+                pure $ (squares !! (shiftL i 1 - 1 .^. bit (m' - 1))) * (squares !! (shiftL j 1 - 2))
+           in flip clearBit 0 $
+                s
+                  + sqr s
+                  + (squares !! (bit m' - 1))
+                    * (1 + sum (take (bit (m' - 2)) $ drop (bit (m' - 2)) quarts))
+        else
+          let y = bit $ bit m' - 1
+              z = artinSchreierRoot $ sqr y + y + n
+           in y + z
--- test/Main.hs
+++ test/Main.hs
@@ -7,6 +7,7 @@
 module Main (main) where
 
 import Control.Monad (unless)
+import Data.Bits
 import Data.Nimber
 import Data.Word (Word64)
 import System.Exit (exitFailure)
@@ -15,26 +16,30 @@ import Test.QuickCheck
 instance Arbitrary Nimber where
   arbitrary = fromIntegral @Word64 . getLarge <$> arbitrary
 
+prop_floorLog :: Positive Int -> Bool
+prop_floorLog Positive {getPositive = n} =
+  floorLog n == floor @Double @Int (logBase 2 $ fromIntegral n)
+
 prop_neg :: Nimber -> Bool
 prop_neg a = a - a == 0
 
 prop_add_id :: Nimber -> Bool
-prop_add_id a = (a + 0) == a && (0 + a == a)
+prop_add_id a = a + 0 == a && 0 + a == a
 
 prop_mult_id :: Nimber -> Bool
-prop_mult_id a = (a * 1) == a && (1 * a == a)
+prop_mult_id a = a * 1 == a && 1 * a == a
 
 prop_assoc_add :: Nimber -> Nimber -> Nimber -> Bool
 prop_assoc_add a b c = a + (b + c) == (a + b) + c
 
-prop_assoc_mul :: Nimber -> Nimber -> Nimber -> Bool
-prop_assoc_mul a b c = a * (b * c) == (a * b) * c
+prop_assoc_mult :: Nimber -> Nimber -> Nimber -> Bool
+prop_assoc_mult a b c = a * (b * c) == (a * b) * c
 
 prop_distrib :: Nimber -> Nimber -> Nimber -> Bool
 prop_distrib a b c = a * (b + c) == a * b + a * c
 
-prop_recip :: Nimber -> Bool
-prop_recip a = a == 0 || a / a == 1
+prop_recip :: NonZero Nimber -> Bool
+prop_recip NonZero {getNonZero = a} = a / a == 1
 
 prop_inv :: Nimber -> Bool
 prop_inv a = a == 0 || recip (recip a) == a
@@ -51,6 +56,9 @@ prop_pow_zero NonNegative {getNonNegative = n} = 0 `pow` n == if n == 0 then 1 e
 prop_sqrt :: Nimber -> Bool
 prop_sqrt a = sqr (sqrt a) == a
 
+prop_as :: Nimber -> Bool
+prop_as a = let x = artinSchreierRoot a in sqr x + x == a && not (x `testBit` 0)
+
 mex :: (Functor f, Foldable f, Eq b, Num b, Num a) => f b -> a
 mex s = if 0 `notElem` s then 0 else 1 + mex (fmap (+ (-1)) s)
 
@@ -64,19 +72,25 @@ nimberSumTable = fmap add <$> [(i,) <$> [0 ..] | i <- [0 ..]]
     add (a, b) = mex $ [nimberSumTable !! a' !! b | a' <- [0 .. a - 1]] ++ [nimberSumTable !! a !! b' | b' <- [0 .. b - 1]]
 
 -- | Compute nimber product directly from the definition.  This is very slow.
-nimberMul :: Int -> Int -> Int
-nimberMul = (!!) . (nimberProdTable !!)
+nimberMult :: Int -> Int -> Int
+nimberMult = (!!) . (nimberProdTable !!)
 
 nimberProdTable :: [[Int]]
-nimberProdTable = fmap mul <$> [(i,) <$> [0 ..] | i <- [0 ..]]
+nimberProdTable = [mult i <$> [0 ..] | i <- [0 ..]]
   where
-    mul (a, b) = mex $ [(nimberProdTable !! a' !! b) `nimberAdd` (nimberProdTable !! a !! b') `nimberAdd` (nimberProdTable !! a' !! b') | a' <- [0 .. a - 1], b' <- [0 .. b - 1]]
+    mult a b = mex $ [(nimberProdTable !! a' !! b) `nimberAdd` (nimberProdTable !! a !! b') `nimberAdd` (nimberProdTable !! a' !! b') | a' <- [0 .. a - 1], b' <- [0 .. b - 1]]
 
--- prop_def_add :: Small Int -> Small Int -> Bool
--- prop_def_add Small {getSmall = a} Small {getSmall = b} = abs a `nimberAdd` abs b == fromIntegral (getNimber $ fromIntegral a + fromIntegral b)
+prop_def_add :: Bool
+prop_def_add = and $ do
+  i <- [0 .. 15]
+  j <- [0 .. 15]
+  pure $ fromIntegral @Int @Nimber (nimberAdd i j) == fromIntegral i + fromIntegral j
 
--- prop_def_mul :: Small Int -> Small Int -> Bool
--- prop_def_mul Small {getSmall = a} Small {getSmall = b} = abs a `nimberMul` abs b == fromIntegral (getNimber $ fromIntegral a * fromIntegral b)
+prop_def_mult :: Bool
+prop_def_mult = and $ do
+  i <- [0 .. 15]
+  j <- [0 .. 15]
+  pure $ fromIntegral @Int @Nimber (nimberMult i j) == fromIntegral i * fromIntegral j
 
 pure []
 
