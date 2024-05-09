d25575cd19b4b7c50308a044b21bd058d131b335
noiioiu <noiioiu@cocaine.ninja>
2024-05-09 18:57:53 -0500

    Change Nimber to FiniteNimber

--- test/Main.hs
+++ test/Main.hs
@@ -13,59 +13,59 @@ import Data.Word (Word64)
 import System.Exit (exitFailure)
 import Test.QuickCheck
 
-instance Arbitrary Nimber where
+instance Arbitrary FiniteNimber where
   arbitrary = fromIntegral @Word64 . getLarge <$> arbitrary
 
 prop_floorLog :: Positive Int -> Bool
 prop_floorLog Positive {getPositive = n} =
   floorLog n == floor @Double @Int (logBase 2 $ fromIntegral n)
 
-prop_neg :: Nimber -> Bool
+prop_neg :: FiniteNimber -> Bool
 prop_neg a = a - a == 0
 
-prop_add_id :: Nimber -> Bool
+prop_add_id :: FiniteNimber -> Bool
 prop_add_id a = a + 0 == a && 0 + a == a
 
-prop_mult_id :: Nimber -> Bool
+prop_mult_id :: FiniteNimber -> Bool
 prop_mult_id a = a * 1 == a && 1 * a == a
 
-prop_comm_add :: Nimber -> Nimber -> Bool
+prop_comm_add :: FiniteNimber -> FiniteNimber -> Bool
 prop_comm_add a b = a + b == b + a
 
-prop_comm_mult :: Nimber -> Nimber -> Bool
+prop_comm_mult :: FiniteNimber -> FiniteNimber -> Bool
 prop_comm_mult a b = a * b == b * a
 
-prop_assoc_add :: Nimber -> Nimber -> Nimber -> Bool
+prop_assoc_add :: FiniteNimber -> FiniteNimber -> FiniteNimber -> Bool
 prop_assoc_add a b c = a + (b + c) == (a + b) + c
 
-prop_assoc_mult :: Nimber -> Nimber -> Nimber -> Bool
+prop_assoc_mult :: FiniteNimber -> FiniteNimber -> FiniteNimber -> Bool
 prop_assoc_mult a b c = a * (b * c) == (a * b) * c
 
-prop_distrib :: Nimber -> Nimber -> Nimber -> Bool
+prop_distrib :: FiniteNimber -> FiniteNimber -> FiniteNimber -> Bool
 prop_distrib a b c = a * (b + c) == a * b + a * c
 
-prop_recip :: NonZero Nimber -> Bool
+prop_recip :: NonZero FiniteNimber -> Bool
 prop_recip NonZero {getNonZero = a} = a / a == 1
 
-prop_inv :: Nimber -> Bool
+prop_inv :: FiniteNimber -> Bool
 prop_inv a = a == 0 || recip (recip a) == a
 
-prop_sqr :: Nimber -> Bool
+prop_sqr :: FiniteNimber -> Bool
 prop_sqr a = sqr a == a * a
 
-prop_pow :: NonZero Nimber -> Integer -> Bool
+prop_pow :: NonZero FiniteNimber -> Integer -> Bool
 prop_pow NonZero {getNonZero = a} n = pow a n == a ^^ n
 
 prop_pow_zero :: NonNegative Integer -> Bool
 prop_pow_zero NonNegative {getNonNegative = n} = 0 `pow` n == if n == 0 then 1 else 0
 
-prop_sqrt :: Nimber -> Bool
+prop_sqrt :: FiniteNimber -> Bool
 prop_sqrt a = sqr (sqrt a) == a
 
-prop_as :: Nimber -> Bool
+prop_as :: FiniteNimber -> Bool
 prop_as a = let x = artinSchreierRoot a in sqr x + x == a && not (x `testBit` 0)
 
-prop_quadratic :: Nimber -> Nimber -> Nimber -> Bool
+prop_quadratic :: FiniteNimber -> FiniteNimber -> FiniteNimber -> Bool
 prop_quadratic p q r =
   let (x, y) = solveQuadratic p q
    in sqr x + p * x + q == 0
@@ -98,13 +98,13 @@ prop_def_add :: Bool
 prop_def_add = and $ do
   i <- [0 .. 15]
   j <- [0 .. 15]
-  pure $ fromIntegral @Int @Nimber (nimberAdd i j) == fromIntegral i + fromIntegral j
+  pure $ fromIntegral @Int @FiniteNimber (nimberAdd i j) == fromIntegral i + fromIntegral j
 
 prop_def_mult :: Bool
 prop_def_mult = and $ do
   i <- [0 .. 15]
   j <- [0 .. 15]
-  pure $ fromIntegral @Int @Nimber (nimberMult i j) == fromIntegral i * fromIntegral j
+  pure $ fromIntegral @Int @FiniteNimber (nimberMult i j) == fromIntegral i * fromIntegral j
 
 pure []
 
